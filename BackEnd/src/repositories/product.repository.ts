76aa4328f44bb4dff276cf2ade323a915bcc52f2
أمال العرفاوي <91819124+emelarfewi@users.noreply.github.com>
2024-01-28 14:34:13 +0100

    Update product.repository.ts

--- BackEnd/src/repositories/product.repository.ts
+++ BackEnd/src/repositories/product.repository.ts
@@ -1,1 +1,16 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {GestionstockDsDataSource} from '../datasources';
+import {Product, ProductRelations} from '../models';
 
+export class ProductRepository extends DefaultCrudRepository<
+  Product,
+  typeof Product.prototype.Id,
+  ProductRelations
+> {
+  constructor(
+    @inject('datasources.gestionstock_ds') dataSource: GestionstockDsDataSource,
+  ) {
+    super(Product, dataSource);
+  }
+}
