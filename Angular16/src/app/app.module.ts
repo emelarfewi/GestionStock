11ed2a05ecd18e55e77403c3c151df07a7b6b474
أمال العرفاوي <91819124+emelarfewi@users.noreply.github.com>
2024-01-28 14:56:31 +0100

    Update app.module.ts

--- Angular16/src/app/app.module.ts
+++ Angular16/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import {HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,12 +13,14 @@ import { AccueilComponent } from './accueil/accueil.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
