3173e7e4e6691a2fa06031ff9c88db3a38754db6
أمال العرفاوي <91819124+emelarfewi@users.noreply.github.com>
2024-01-28 11:52:40 +0100

    Update accueil.component.ts

--- Angular16/src/app/accueil/accueil.component.ts
+++ Angular16/src/app/accueil/accueil.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserService } from 'path-to-your-user-service';  // Import your user service
+import { UserService } from './../services/user.service';
 
 @Component({
   selector: 'app-accueil',
