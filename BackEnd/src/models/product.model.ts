7a0ec1f5ea453b7f70edcf69702b0ac669cef50e
أمال العرفاوي <91819124+emelarfewi@users.noreply.github.com>
2024-01-28 14:36:37 +0100

    Create product.model.ts

---
+++ BackEnd/src/models/product.model.ts
@@ -0,0 +1,21 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Product extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  Id?: number;
+
+  constructor(data?: Partial<Product>) {
+    super(data);
+  }
+}
+
+export interface ProductRelations {
+  // describe navigational properties here
+}
+
+export type ProductWithRelations = Product & ProductRelations;
