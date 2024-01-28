0b9efbba4b81b4b3f8daca3919d653086e12cc89
أمال العرفاوي <91819124+emelarfewi@users.noreply.github.com>
2024-01-28 12:26:21 +0100

    Update user.service.ts

--- Angular16/src/app/services/user.service.ts
+++ Angular16/src/app/services/user.service.ts
@@ -6,15 +6,28 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { public http: HttpClient }
-
-  addUser() {
+  //L'Url de l'Api
+  apiUrl = "http://localhost:3000/users";
+  
+  constructor( public http: HttpClient ) { }
+  
+  addUser(u: User) {
+    return this.http.post<any>(this.apiUrl, u);
   }
 
-  findByEmail () {
+  findByEmail (e: string) {
+    let Url = this.apiUrl + "/" + e;
+    return this.http.get<User>(Url);
   }
 
-  findByEmailPwd () { 
+  findByEmailPwd (e: string, p: string) { 
+    let Url = this.apiUrl + "/" + e + "/" + p;
+    return this.http.get<User>(Url);
   }
+
+  changePwd (u: User, e: string) {
+    let Url = this.apiUrl + "/" + e;
+    return this.http.put(Url, u):
+  }
+  
 }
