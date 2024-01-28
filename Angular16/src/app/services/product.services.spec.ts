5bf276a058f4df1595044f93f407b67a95b5b548
أمال العرفاوي <91819124+emelarfewi@users.noreply.github.com>
2024-01-28 14:30:32 +0100

    Update and rename produit.services.spec.ts to product.services.spec.ts

--- Angular16/src/app/services/product.services.spec.ts
+++ Angular16/src/app/services/product.services.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProduitService } from './produit.service';
+import { ProductService } from './product.service';
 
-describe('ProduitService', () => {
-  let service: ProduitService;
+describe('ProductService', () => {
+  let service: ProductService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProduitService);
+    service = TestBed.inject(ProductService);
   });
 
   it('should be created', () => {
