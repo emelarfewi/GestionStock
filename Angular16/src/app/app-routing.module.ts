06568ba0a47653b973a047d681e44ee17b156b1c
أمال العرفاوي <91819124+emelarfewi@users.noreply.github.com>
2024-01-28 11:45:18 +0100

    Update app-routing.module.ts

--- Angular16/src/app/app-routing.module.ts
+++ Angular16/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AccueilComponent } from './accueil/accueil.component';
+
 const routes: Routes = [
   {path: '', component:LoginComponent},
   {path: '/signup', component:SignupComponent},
-  {path: '/accueil', component:AcueilComponent}
+  {path: '/accueil', component:AccueilComponent}
 ];
 
 @NgModule({
