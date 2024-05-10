48a10fb58cd4d2ec82b9d36c51ceaa1aed7e51d8
أمال العرفاوي <91819124+emelarfewi@users.noreply.github.com>
2024-05-10 17:19:43 +0100

    Delete BackEnd SpringBoot directory

--- BackEnd SpringBoot/src/main/java/com/example/BackEnd/repositories/FournisseurRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BackEnd.repositories;
-import com.example.BackEnd.models.Fournisseur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FournisseurRepository extends JpaRepository<Fournisseur, Long>{
-    void deleteFournisseurByIdFrn(Long id);
-    Fournisseur findFournisseurByIdFrn(Long id);
-}
